1f86d4e3431c48e869fdc668718d7fdc2c7bde20
Muhammed Fiban <136978055+MuhammedFiban@users.noreply.github.com>
2024-03-18 16:34:52 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -146,7 +146,7 @@ app.post('/load-application-details', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
